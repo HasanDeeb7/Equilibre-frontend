17f6fb2e0711f092e378a7b20ca612fa03d339a5
Layla Hammoud <laylahammoud63@gmail.com>
2024-01-20 17:28:11 +0200

    create all products layout and fetch the products

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import style from "./Products.module.css";
+import SearchBar from "../../components/SearchBar/SearchBar";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import ProductCard from "../../components/ProductCard/ProductCard";
+const Products = () => {
+  const [Products, setProducts] = useState();
+  const [loading, setLoading] = useState(false);
 
-function Products() {
+  useEffect(() => {
+    async function getProducts() {
+      try {
+        setLoading(true);
+        const response = await axios.get(
+          `${process.env.REACT_APP_ENDPOINT}product/AllProducts`
+        );
+        if (response) {
+          console.log(response.data.data);
+          setProducts(response.data.data);
+          setLoading(false);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getProducts();
+  }, []);
   return (
-    <div>Products</div>
-  )
-}
+    <>
+      <main className={style.mainContainer}>
+        <section className={style.categorySection}></section>
+        <section className={style.cardsContainer}>
+          <SearchBar />
+          <div className={style.cardsWrapper}>
+          {Products &&
+            Products.map((product, index) => (
+              <ProductCard
+              id={product._id}
+                key={index}
+                name={product.name}
+                description={product.description}
+                price={product.soldQuantityCounter}
+                imgurl={product.image}
+              />
+            ))}
+            </div>
+        </section>
+      </main>
+    </>
+  );
+};
 
-export default Products
+export default Products;
