c8976ca77f9d5dae5c7946bf1b710424a3fb647e
Layla Hammoud <laylahammoud63@gmail.com>
2024-01-20 12:53:53 +0200

    finish contact us page

---
+++ src/pages/ContactUsPage/ContactUsPage.js
@@ -0,0 +1,14 @@
+import ContactUsForm from "../../components/ContactUsForm/ContactUsForm"
+import HeaderContactUs from "../../components/HeaderContactUs/HeaderContactUs"
+import ContactInfo from "../../components/ContactInfoSection/ContactInfo"
+
+const ContactUsPage =()=> {
+  return (
+    <>
+      <HeaderContactUs/>
+      <ContactInfo/>
+      <ContactUsForm/>
+    </>
+  )
+}
+export default ContactUsPage
