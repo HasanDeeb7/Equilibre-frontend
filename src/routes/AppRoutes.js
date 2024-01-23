79d0809f1625c79e8d2db8f86fa117f83906efa4
jammal.souheir@gmail.com <jammal.souheir@gmail.com>
2024-01-23 10:50:04 +0200

    changing routes

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -17,43 +17,45 @@ import UserProfile from "../pages/UserProfile/UserProfile";
 import Sidebar from "../components/Sidebar/Sidebar";
 import { AnimatePresence } from "framer-motion";
 import EditeUserProfile from "../components/UeserProfile/UserProfile";
-import ShippingPage from "../pages/shipping/ShippingPage";
 function AppRoutes() {
   const { user } = useUserStore();
   const location = useLocation();
   return (
     <div>
-      <Routes>
-        <Route  element={<NavBar />}>
-          <Route path="/" element={<ConsultingSection />}></Route>
-          <Route path="/products" element={<Products />}></Route>
-          <Route path="/consultation" element={<Products />}></Route>
-          <Route path="/profile" element={<Products />}></Route>
-          <Route path="/single" element={<SingleProduct />}></Route>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route element={<NavBar />}>
+            <Route path="/" element={<ConsultingSection key="home" />}></Route>
+            <Route path="/products" element={<Products />}></Route>
+            <Route
+              path="/consultation"
+              element={<Products key="products" />}
+            ></Route>
+            {/* <Route path="/profile" element={<Products />}></Route> */}
+            <Route path="/single" element={<SingleProduct />}></Route>
+            <Route path="/cart" element={<Cart />}></Route>
+            <Route path="/about" element={<AboutUs />}></Route>
+            <Route path="/checkout" element={<Checkout />}></Route>
+          </Route>
+
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
           <Route path="/cart" element={<Cart />}></Route>
           <Route path="/about" element={<AboutUs />}></Route>
           <Route path="/checkout" element={<Checkout />}></Route>
-          <Route path="/shipping" element={<ShippingPage />}></Route>
-        </Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/signup" element={<Signup />}></Route>
-        <Route path="/cart" element={<Cart />}></Route>
-        <Route path="/about" element={<AboutUs />}></Route>
-        <Route path="/checkout" element={<Checkout />}></Route>
-        <Route path="/dashboard" element={<Sidebar />}>
-
-          <Route
-            path="/dashboard"
-            element={
-              // <ProtectedRoute isAllowed={user}>
-              <Dashboard />
-              // </ProtectedRoute>
-            }
-          ></Route>
-        </Route>
-        <Route path="/*" element={<NotFound />}></Route>
-
-      </Routes>
+          <Route path="/dashboard" element={<Sidebar />}>
+            <Route
+              path="/dashboard"
+              element={
+                // <ProtectedRoute isAllowed={user}>
+                <Dashboard />
+                // </ProtectedRoute>
+              }
+            ></Route>
+          </Route>
+          <Route path="/*" element={<NotFound />}></Route>
+        </Routes>
+      </AnimatePresence>
     </div>
   );
 }
