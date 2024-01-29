2f91cf5a08c7a664fdd5f80179192e406370bd2b
anwar khawli <anwarkhawle@gmail.com>
2024-01-29 09:32:21 +0200

    nthg to commit

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,10 +3,31 @@ import TopSellerHome from "../../components/TopSellerHome/TopSellerHome";
 import AboutUsSection from '../../components/AbouUsSection/AboutUsSection'
 import ConsultingSection from '../../components/ConsultingSection/ConsultingSection'
 import TestimonialSection from '../../components/TestimonialSection/TestimonialSection'
+import GlobalOffer from "../../components/GlobalOffer/GlobalOffer";
+import { useGlobalOfferStore } from "../../Store";
+import axios from "axios";
+import { useEffect } from "react";
 import { Helmet } from 'react-helmet-async';
 function Home() {
+  const { offer, setOffer } = useGlobalOfferStore();
+  useEffect(() => {
+      async function getOffers() {
+        try {
+          const response = await axios.get(
+            `${process.env.REACT_APP_ENDPOINT}globalOffer/activeOffer`
+          );
+          if (response.data[0]) {
+              setOffer(response.data[0]);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      getOffers();
+    }, []);
   return (
     <>
+    {offer && <GlobalOffer offer={offer}/>}
       <Helmet>
         <title>Equilibre - home</title>
         <meta name="decription" content="" />
